1b7d223bebeda6fd561bc5d3a9b7904684f36472
Denchik1143 <144147611+Denchik1143@users.noreply.github.com>
2023-11-28 01:10:01 +0200

    Add files via upload

--- site/app.py
+++ site/app.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    search_query = "Search the world's largest fan vlog platform"  # Замініть на ваші дані або логіку
+    return render_template('index.html', search_query=search_query)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-from flask_sqlalchemy import SQLAlchemy
-
