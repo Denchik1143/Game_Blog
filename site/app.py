110a2be565573e53a7a9b919b1ac8e1270c28ca6
Denchik1143 <144147611+Denchik1143@users.noreply.github.com>
2023-12-02 14:11:38 +0200

    Add files via upload

--- site/app.py
+++ site/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 def index():
     search_query = "Search the world's largest fan vlog platform"  # Замініть на ваші дані або логіку
     return render_template('index.html', search_query=search_query)
+
+@app.route('/form')
+def show_form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Отримайте дані з форми
+    title = request.form.get('title')
+    image = request.files['image']
+    text = request.form.get('text')
 
 if __name__ == '__main__':
     app.run(debug=True)
